7efd440ba5820dfbd543fab73bf23b612691191e
ydroulis <lombardi.droulis@gmail.com>
2021-06-22 18:44:17 -0300

    create background linear component

---
+++ src/components/Background/style.ts
@@ -0,0 +1,8 @@
+import { StyleSheet } from "react-native";
+import { theme } from "../../global/Styles/theme";
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    }
+});
