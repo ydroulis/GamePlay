de721e7eab694ed4253beb6a6ead6554d783802d
ydroulis <lombardi.droulis@gmail.com>
2021-06-22 18:45:21 -0300

    create signin screen

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { View, Text, Image } from 'react-native';
+import { ButtonIcon } from '../../components/ButtonIcon';
+
+import IlustrationImg from '../../assets/illustration.png'
+import { styles } from './styles'
+
+export function SignIn() {
+    return (
+        <View style={styles.container}>
+            <Image source={IlustrationImg} style={styles.image} resizeMode="stretch"/>
+
+            <View style={styles.content}>
+                <Text style={styles.title}>
+                    Conecte-se {`\n`}
+                    e organize suas {`\n`}
+                    jogatinas
+                </Text>
+                <Text style={styles.subtitle}>
+                    Crie Grupos para jogar seus games {`\n`}
+                    favoritos com seus amigos
+                </Text>
+
+                <ButtonIcon title="Entrar com Discord" activeOpacity={0.7}/>
+            </View>
+
+        </View>
+    );
+}
