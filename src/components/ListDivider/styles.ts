d961f9db867df42fc57b907f5be467b44ee0c6e4
ydroulis <lombardi.droulis@gmail.com>
2021-06-23 09:00:47 -0300

    create listdivider component

---
+++ src/components/ListDivider/styles.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+import { theme } from "../../global/Styles/theme";
+
+export const styles = StyleSheet.create({
+    container: {
+        width: '80%',
+        height: '1px',
+        backgroundColor: theme.colors.secondary40,
+        marginVertical: 21,
+        alignSelf: 'flex-end',
+    }
+})
