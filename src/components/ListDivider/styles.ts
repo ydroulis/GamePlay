9051795e8fc335524b1690f9db4c7741959a5456
ydroulis <lombardi.droulis@gmail.com>
2021-06-23 09:25:32 -0300

    fix problem with listdivider

--- src/components/ListDivider/styles.ts
+++ src/components/ListDivider/styles.ts
@@ -4,7 +4,7 @@ import { theme } from "../../global/Styles/theme";
 export const styles = StyleSheet.create({
     container: {
         width: '80%',
-        height: '1px',
+        height: 1,
         backgroundColor: theme.colors.secondary40,
         marginVertical: 21,
         alignSelf: 'flex-end',
