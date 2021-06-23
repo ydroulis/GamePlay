1e900b139abcb208e1fafad61e25b839ad756cb6
ydroulis <lombardi.droulis@gmail.com>
2021-06-23 09:00:29 -0300

    create categoryselect component

---
+++ src/components/CategorySelect/styles.ts
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+import { theme } from "../../global/Styles/theme";
+
+export const styles = StyleSheet.create({
+    container: {
+        minHeight: 120,
+        maxHeight: 120,
+        paddingLeft: 24,
+    }
+})
