368dc801ca91b1dbc5764991d720f3b20a7c8690
ydroulis <lombardi.droulis@gmail.com>
2021-06-23 08:59:48 -0300

    refac buttonicon

--- src/components/ButtonIcon/index.tsx
+++ src/components/ButtonIcon/index.tsx
@@ -1,21 +1,22 @@
 import React from 'react';
-import { Text, Image, View, TouchableOpacity, TouchableOpacityProps } from 'react-native';
+import { Text, Image, View } from 'react-native';
+import { RectButton, RectButtonProps } from 'react-native-gesture-handler';
 
 import DiscordImg from '../../assets/discord.png'
 import { styles } from './styles'
 
-type Props = TouchableOpacityProps & {
+type Props = RectButtonProps & {
     title: string;
 }
 
-export function ButtonIcon({title, activeOpacity} : Props){
+export function ButtonIcon({title, ...rest} : Props){
     return(
-        <TouchableOpacity activeOpacity={activeOpacity} style={styles.container}>
+        <RectButton  style={styles.container} {...rest}>
             <View style={styles.iconWrapper}>
                 <Image source={DiscordImg} style={styles.icon}/>
             </View>
 
             <Text style={styles.title}>{title}</Text>
-        </TouchableOpacity>
+        </RectButton>
     )
 }
