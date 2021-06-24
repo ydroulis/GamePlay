986bb9813fa36941919f64b14d4bf33399326fc3
ydroulis <lombardi.droulis@gmail.com>
2021-06-24 08:13:36 -0300

    create smallinput component

---
+++ src/components/SmallImput/styles.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+import { theme } from "../../global/Styles/theme";
+
+export const styles = StyleSheet.create({
+    container: {
+        width: 48,
+        height: 48,
+        backgroundColor: theme.colors.secondary40,
+        borderRadius: 8,
+        color: theme.colors.heading,
+        fontFamily: theme.fonts.text400,
+        fontSize: 13,
+        marginRight: 4,
+        textAlign: 'center',
+    },
+})
