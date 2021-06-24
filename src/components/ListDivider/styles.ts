e8dd06f092ba6394b06a6d83db2c91eac4278132
ydroulis <lombardi.droulis@gmail.com>
2021-06-24 08:10:12 -0300

    refac listdivider style

--- src/components/ListDivider/styles.ts
+++ src/components/ListDivider/styles.ts
@@ -3,10 +3,11 @@ import { theme } from "../../global/Styles/theme";
 
 export const styles = StyleSheet.create({
     container: {
-        width: '80%',
+        width: '75%',
         height: 1,
         backgroundColor: theme.colors.secondary40,
-        marginVertical: 21,
+        marginVertical: 31,
+        marginTop: 2,
         alignSelf: 'flex-end',
     }
 })
