f5349386ed87790af58c9df52219d3ca9037e70b
ydroulis <lombardi.droulis@gmail.com>
2021-06-24 08:18:40 -0300

    add background component

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text, Image } from 'react-native';
 import { ButtonIcon } from '../../components/ButtonIcon';
+import { Background } from '../../components/Background';
 
 import IlustrationImg from '../../assets/illustration.png'
 import { styles } from './styles'
@@ -10,29 +11,30 @@ import { useNavigation } from '@react-navigation/native';
 export function SignIn() {
     const navigation = useNavigation();
 
-    function handleSignIn(){
+    function handleSignIn() {
         navigation.navigate('Home');
         console.log("Home");
     }
 
     return (
-        <View style={styles.container}>
-            <Image source={IlustrationImg} style={styles.image} resizeMode="stretch"/>
+        <Background>
+            <View style={styles.container}>
+                <Image source={IlustrationImg} style={styles.image} resizeMode="stretch" />
 
-            <View style={styles.content}>
-                <Text style={styles.title}>
-                    Conecte-se {`\n`}
-                    e organize suas {`\n`}
-                    jogatinas
-                </Text>
-                <Text style={styles.subtitle}>
-                    Crie Grupos para jogar seus games {`\n`}
-                    favoritos com seus amigos
-                </Text>
+                <View style={styles.content}>
+                    <Text style={styles.title}>
+                        Conecte-se {`\n`}
+                        e organize suas {`\n`}
+                        jogatinas
+                    </Text>
+                    <Text style={styles.subtitle}>
+                        Crie Grupos para jogar seus games {`\n`}
+                        favoritos com seus amigos
+                    </Text>
 
-                <ButtonIcon title="Entrar com Discord" activeOpacity={0.7} onPress={handleSignIn}/>
+                    <ButtonIcon title="Entrar com Discord" activeOpacity={0.7} onPress={handleSignIn} />
+                </View>
             </View>
-
-        </View>
+        </Background>
     );
 }
