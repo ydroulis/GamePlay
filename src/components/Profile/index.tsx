de74fbc8769f820fee501b302692a683f40aed69
ydroulis <lombardi.droulis@gmail.com>
2021-06-24 08:11:18 -0300

    fix avatar import

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import { styles } from './styles';
-import Avatar from '../Avatar';
+import { Avatar } from '../Avatar';
 
 export function Profile() {
     return (
