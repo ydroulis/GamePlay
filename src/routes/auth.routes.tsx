a7e36eb7b9fed1bd6f64de4130f72b17cd2fea3b
ydroulis <lombardi.droulis@gmail.com>
2021-06-24 08:14:58 -0300

    add new routes

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -3,14 +3,27 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 import { Home } from '../screens/Home';
 import { SignIn } from '../screens/SignIn';
+import { AppointmentDetails } from '../screens/AppointmentDetails';
+import { AppointmentCreate } from '../screens/AppointmentCreate';
+import { theme } from '../global/Styles/theme';
 
 const { Navigator, Screen } = createStackNavigator();
 
-export function AuthRoutes(){
-    return(
-        <Navigator headerMode="none" screenOptions={{ cardStyle: { backgroundColor: 'transparent'}}}>
-            <Screen name="SignIn" component={SignIn}/>
-            <Screen name="Home" component={Home}/>
+export function AuthRoutes() {
+    return (
+        <Navigator
+            headerMode="none"
+            screenOptions={{
+                cardStyle: {
+                    backgroundColor: theme.colors.secondary100
+                }
+            }}
+        >
+            <Screen name="SignIn" component={SignIn} />
+            <Screen name="Home" component={Home} />
+            <Screen name="AppointmentDetails" component={AppointmentDetails} />
+            <Screen name="AppointmentCreate" component={AppointmentCreate} />
+
         </Navigator>
     )
 }
