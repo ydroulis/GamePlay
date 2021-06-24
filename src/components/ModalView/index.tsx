0c9874f68e699d744c133bdd0c760bf921f8b510
ydroulis <lombardi.droulis@gmail.com>
2021-06-24 08:10:58 -0300

    create modalview component

---
+++ src/components/ModalView/index.tsx
@@ -0,0 +1,23 @@
+import React, { Children, ReactNode } from "react";
+import { View, Text, Modal, ModalProps } from "react-native";
+import { styles } from "./styles";
+import { Background } from "../Background";
+
+type Props = ModalProps & {
+    children: ReactNode;
+}
+
+export function ModalView({ children, ...rest }: Props) {
+    return (
+        <Modal transparent animationType="slide" {...rest}>
+            <View style={styles.overlay}>
+                <View style={styles.container}>
+                    <Background>
+                        <View style={styles.bar}/>
+                        {children}
+                    </Background>
+                </View>
+            </View>
+        </Modal>
+    )
+}
