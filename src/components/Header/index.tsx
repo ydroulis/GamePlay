fd3a08d23aa68bf8c64c20aad7a00851f9807b26
ydroulis <lombardi.droulis@gmail.com>
2021-06-25 08:36:27 -0300

    set conditional render

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -39,10 +39,12 @@ export function Header({ title, action }: Props) {
                 {title}
             </Text>
             {
-                action &&
+                action ?
                 <View>
                     {action}
                 </View>
+                :
+                <View style={{ width: 24 }}/>
             }
         </LinearGradient>
     )
