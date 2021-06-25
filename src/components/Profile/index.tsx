245b363d04f9cdda6d7040ce365c92fcf8852a64
ydroulis <lombardi.droulis@gmail.com>
2021-06-25 08:40:12 -0300

    set dynamic data

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -2,18 +2,21 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import { styles } from './styles';
 import { Avatar } from '../Avatar';
+import { useAuth } from '../../hooks/auth';
 
 export function Profile() {
+const { user } = useAuth();
+
     return (
         <View style={styles.container}>
-            <Avatar urlImage="https://github.com/ydroulis.png"/>
+            <Avatar urlImage={user.avatar}/>
             <View>
                 <View style={styles.user}>
                     <Text style={styles.greeting}>
                         Olá,
                     </Text>
                     <Text style={styles.username}>
-                        Yuri
+                        {user.firstName}
                     </Text>
                 </View>
                 <Text style={styles.message}>
