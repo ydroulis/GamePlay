f710cbfe32f1e3f4d8ca34f0611b34a02a616c0d
ydroulis <lombardi.droulis@gmail.com>
2021-06-25 08:32:49 -0300

    use auth provider and logbox

--- App.tsx
+++ App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import { StatusBar } from 'react-native';
+import { StatusBar, LogBox } from 'react-native';
 import { useFonts } from 'expo-font';
 import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
 import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani';
 import AppLoading from 'expo-app-loading'
 import { Background } from './src/components/Background';
 import { Routes } from './src/routes';
+import { AuthProvider } from './src/hooks/auth';
 
 
+LogBox.ignoreLogs(['You are not currently signed in to Expo on your development machine.'])
 export default function App() {
 
   const [fontsLoaded] = useFonts({
@@ -24,7 +26,9 @@ export default function App() {
   return (
     <Background>
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
-      <Routes/>
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
     </Background>
   );
 }
