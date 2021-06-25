dcb132e585a7e5c2e32171d0c04621781c6b40f3
ydroulis <lombardi.droulis@gmail.com>
2021-06-25 08:34:04 -0300

    use linear gradient

--- src/components/Appointment/index.tsx
+++ src/components/Appointment/index.tsx
@@ -3,17 +3,12 @@ import { RectButton, RectButtonProps } from 'react-native-gesture-handler';
 import { View, Text } from 'react-native';
 import { styles } from './styles';
 import { GuildIcon } from '../GuildIcon';
+import { LinearGradient } from 'expo-linear-gradient';
 import { categories } from '../../utils/categories';
 import PlayerSvg from '../../assets/player.svg'
 import CalendarSvg from '../../assets/calendar.svg'
 import { theme } from '../../global/Styles/theme';
-
-export type GuildProps = {
-    id: string;
-    name: string;
-    icon: null,
-    owner: boolean,
-}
+import { GuildProps } from '../Guild';
 
 export type AppoitmentProps = {
     id: string;
@@ -31,12 +26,14 @@ export function Appointment({ data, ...rest }: Props) {
 
     const [category] = categories.filter(item => item.id === data.category)
     const { owner } = data.guild;
-    const { primary, on } = theme.colors
+    const { primary, on, secondary50, secondary70 } = theme.colors
 
     return (
         <RectButton {...rest}>
             <View style={styles.container}>
-                <GuildIcon />
+                <LinearGradient style={styles.guildIconContainer} colors={[  secondary50, secondary70]}>
+                    <GuildIcon />
+                </LinearGradient>
 
                 <View style={styles.content}>
                     <View style={styles.header}>
