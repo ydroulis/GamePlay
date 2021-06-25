7471c5a163670bfd5630c84764ea56585f375729
ydroulis <lombardi.droulis@gmail.com>
2021-06-25 08:46:22 -0300

    refac home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -41,49 +41,92 @@ export function Home() {
             date: '22/06 ás 20:40',
             description: 'É hoje que vamos chegar ao challenger sem perder uma partida da m10'
         },
+        {
+            id: '3',
+            guild: {
+                id: '1',
+                name: 'Lendários',
+                icon: null,
+                owner: true,
+            },
+            category: '1',
+            date: '22/06 ás 20:40',
+            description: 'É hoje que vamos chegar ao challenger sem perder uma partida da m10'
+        },
+        {
+            id: '4',
+            guild: {
+                id: '1',
+                name: 'Lendários',
+                icon: null,
+                owner: true,
+            },
+            category: '1',
+            date: '22/06 ás 20:40',
+            description: 'É hoje que vamos chegar ao challenger sem perder uma partida da m10'
+        }, {
+            id: '5',
+            guild: {
+                id: '1',
+                name: 'Lendários',
+                icon: null,
+                owner: true,
+            },
+            category: '1',
+            date: '22/06 ás 20:40',
+            description: 'É hoje que vamos chegar ao challenger sem perder uma partida da m10'
+        }, {
+            id: '6',
+            guild: {
+                id: '1',
+                name: 'Lendários',
+                icon: null,
+                owner: true,
+            },
+            category: '1',
+            date: '22/06 ás 20:40',
+            description: 'É hoje que vamos chegar ao challenger sem perder uma partida da m10'
+        },
     ]
 
     function handleCategorySelect(categoryId: string) {
         categoryId === category ? setCategory('') : setCategory(categoryId);
     }
 
-    function handleAppointmentDetails(){
+    function handleAppointmentDetails() {
         navigation.navigate('AppointmentDetails')
     }
 
-    function handleAppointmentCreate(){
+    function handleAppointmentCreate() {
         navigation.navigate('AppointmentCreate')
     }
 
     return (
         <Background>
-            <View style={styles.container}>
-                <View style={styles.header}>
-                    <Profile />
-                    <ButtonAdd onPress={handleAppointmentCreate}/>
-                </View>
-
-
-                <CategorySelect categorySelected={category} setCategory={handleCategorySelect} />
-
-                <View style={styles.content}>
-                    <ListHeader title="Partidas agendadas" subtitle="Total 6" />
-
-                    <FlatList
-                        data={appointments}
-                        keyExtractor={item => item.id}
-                        renderItem={({ item }) => (
-                            <Appointment 
-                                data={item}
-                                onPress={handleAppointmentDetails}
-                            />
-                        )}
-                        ItemSeparatorComponent={() => <ListDivider />}
-                        style={styles.matches}
-                        showsVerticalScrollIndicator={false}
-                    />
-                </View>
+            <View style={styles.header}>
+                <Profile />
+                <ButtonAdd onPress={handleAppointmentCreate} />
             </View>
+
+
+            <CategorySelect categorySelected={category} setCategory={handleCategorySelect} />
+
+
+            <ListHeader title="Partidas agendadas" subtitle="Total 6" />
+            <FlatList
+                data={appointments}
+                keyExtractor={item => item.id}
+                contentContainerStyle={{ paddingBottom: 69 }}
+                renderItem={({ item }) => (
+                    <Appointment
+                        data={item}
+                        onPress={handleAppointmentDetails}
+                    />
+                )}
+                ItemSeparatorComponent={() => <ListDivider />}
+                style={styles.matches}
+                showsVerticalScrollIndicator={false}
+            />
         </Background>
     )
 }
