0671662c6b1b1c23901b4d065b94d48d72abaca8
ydroulis <lombardi.droulis@gmail.com>
2021-06-25 08:34:58 -0300

    use statusbar

--- src/components/Background/index.tsx
+++ src/components/Background/index.tsx
@@ -1,6 +1,6 @@
 import React, { ReactNode } from 'react';
 import { LinearGradient } from 'expo-linear-gradient';
-
+import { StatusBar } from 'react-native';
 import { styles } from './styles';
 import { theme } from '../../global/Styles/theme';
 
@@ -13,6 +13,7 @@ export function Background( { children }: Props ){
     const { secondary80, secondary100 } = theme.colors
     return(
         <LinearGradient style={styles.container} colors={[secondary80, secondary100]}>
+            <StatusBar backgroundColor='transparent' translucent/>
             {children}
         </LinearGradient>
     )
