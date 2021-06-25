7ed530ba26dd05f6f39cd1f7d42164febd88ed50
ydroulis <lombardi.droulis@gmail.com>
2021-06-25 08:41:36 -0300

    create variables file for discord

---
+++ src/configs/discordAuth.ts
@@ -0,0 +1,16 @@
+ const REDIRECT_URI = 'https://auth.expo.io/@anonymous/GamePLay-39c9acb0-0a5b-480a-ba3f-e2e66c267133';
+ const SCOPE = 'identify%20email%20connections%20guilds';
+ const RESPONSE_TYPE = 'token';
+ const CLIENT_ID = '857703047492010055';
+ const CDN_IMAGE = 'https://cdn.discordapp.com';
+
+ 
+
+ export {
+    REDIRECT_URI,
+    SCOPE,
+    RESPONSE_TYPE,
+    CLIENT_ID,
+    CDN_IMAGE,
+ }
+ 
